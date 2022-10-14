26b283a0c590613b9b6b80c7f3408b528743aab1
Steven Rufino <Steven Rufino@DESKTOP-MC0O4VE>
2022-10-14 01:11:18 -0400

    Add project files.

---
+++ ExcelWorkbook2/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelWorkbook2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Registrar_Click(object sender, EventArgs e)
+        {
+
+            var sheet1 = Globals.Sheet1;
+            var r = sheet1.Range["A1"];
+            long f = r.CurrentRegion.Rows.Count + 1;
+
+            sheet1.Cells[f, 1].Value = textBox1.Text;
+            sheet1.Cells[f, 2].Value = textBox2.Text;
+            sheet1.Cells[f, 3].Value = textBox3.Text;
+            sheet1.Cells[f, 4].Value = textBox4.Text;
+            sheet1.Cells[f, 5].Value = textBox5.Text;
+
+
+
+            this.textBox1.Text = "";
+            this.textBox2.Text = "";
+            this.textBox3.Text = "";
+            this.textBox4.Text = "";
+            this.textBox5.Text = "";
+
+
+
+            this.textBox1.Select();
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ ExcelWorkbook2/Sheet1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+
+namespace ExcelWorkbook2
+{
+    public partial class Sheet1
+    {
+        private void Sheet1_Startup(object sender, System.EventArgs e)
+        {
+
+            MessageBox.Show("Bienvenido aqui insertaras los campos requeridos para conectarte al Web Service");
+
+            var sheetActive = (Excel.Worksheet)Globals.ThisWorkbook.ActiveSheet;
+
+            sheetActive.Range["A1"].Value = "User";
+            sheetActive.Range["B1"].Value = "Password";
+            sheetActive.Range["c1"].Value = "Port";
+            sheetActive.Range["D1"].Value = "Protocolo";
+            sheetActive.Range["E1"].Value = "Url";
+
+
+
+
+        }
+
+        private void Sheet1_Shutdown(object sender, System.EventArgs e)
+        {
+
+
+            MessageBox.Show("Adios");
+
+
+
+
+        }
+
+        #region VSTO Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.Startup += new System.EventHandler(this.Sheet1_Startup);
+            this.Shutdown += new System.EventHandler(this.Sheet1_Shutdown);
+
+        }
+
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form miFormulario = new Form1();
+
+            miFormulario.ShowDialog();
+
+
+        }
+    }
+}
